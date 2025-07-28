cd4adf6fdd202882681803650465a57dacb53925
FaiSaL-YouSaF <faisalyousaf777@gmail.com>
2025-07-29 01:46:57 +0500

    initial commit

--- app/src/main/java/com/faisalyousaf777/clock/MainActivity.java
+++ app/src/main/java/com/faisalyousaf777/clock/MainActivity.java
@@ -10,14 +10,11 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.faisalyousaf777.clock.fragment.AlarmFragment;
-import com.faisalyousaf777.clock.fragment.AlarmToolbarFragment;
 import com.faisalyousaf777.clock.fragment.StopwatchFragment;
-import com.faisalyousaf777.clock.fragment.StopwatchToolbarFragment;
 import com.faisalyousaf777.clock.fragment.TimerFragment;
-import com.faisalyousaf777.clock.fragment.TimerToolbarFragment;
 import com.faisalyousaf777.clock.fragment.WorldClockFragment;
-import com.faisalyousaf777.clock.fragment.WorldClockToolbarFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,34 +30,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        bottomNavigation = findViewById(R.id.bottomNavigation);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
 
-        loadToolbarFragment(AlarmToolbarFragment.newInstance());
-        loadContentFragment(AlarmFragment.newInstance());
-
-
-        bottomNavigation.setOnItemSelectedListener(item -> {
+        NavigationBarView.OnItemSelectedListener itemSelectedListener = item -> {
             if (item.getItemId() == R.id.btnAlarmItem) {
-                loadToolbarFragment(AlarmToolbarFragment.newInstance());
                 loadContentFragment(AlarmFragment.newInstance());
             } else if (item.getItemId() == R.id.btnWorldClockItem) {
-                loadToolbarFragment(WorldClockToolbarFragment.newInstance());
                 loadContentFragment(WorldClockFragment.newInstance());
             } else if (item.getItemId() == R.id.btnStopwatchItem) {
-                loadToolbarFragment(StopwatchToolbarFragment.newInstance());
                 loadContentFragment(StopwatchFragment.newInstance());
             } else if (item.getItemId() == R.id.btnTimerItem) {
-                loadToolbarFragment(TimerToolbarFragment.newInstance());
                 loadContentFragment(TimerFragment.newInstance());
             } else {
                 return false;
             }
             return true;
-        });
-    }
+        };
 
-    private void loadToolbarFragment(Fragment toolbarFragment) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.toolbarContainer, toolbarFragment).commit();
+        bottomNavigation.setOnItemSelectedListener(itemSelectedListener);
     }
 
     private void loadContentFragment(Fragment contentFragment) {
